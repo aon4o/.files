690fcd0a6744617b98a98c83571949614e5e3b37
Alex <48474870+aon2003@users.noreply.github.com>
2023-09-24 15:15:48 +0300

    Update linux_dev.sh
    Added Docker installation

--- linux_dev.sh
+++ linux_dev.sh
@@ -55,4 +55,13 @@ sudo mv composer.phar /usr/local/bin/composer
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 nvm i stable
 
+# Installing Docker
+sudo apt install apt-transport-https ca-certificates curl gnupg -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -aG docker ${USER}
+
 echo "Done!"
+echo "REBOOT NEEDED!"
